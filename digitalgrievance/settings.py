9c0e061e211e29b938aa3e9ad942ad0202688478
Saurav Tamrakar <tamrakarsurav101@gmail.com>
2026-07-22 18:56:22 +0545

    feat: update notice marquee to display real notices

--- digitalgrievance/settings.py
+++ digitalgrievance/settings.py
@@ -68,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'client.context_processors.marquee_context',
             ],
         },
     },
