2c80b1d730ccb4c91cbe39de9db68f1e96395758
Saurav Tamrakar <tamrakarsurav101@gmail.com>
2026-07-28 13:32:07 +0545

    feat: add live reload feature in the web application

--- digitalgrievance/settings.py
+++ digitalgrievance/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "client",
     "server",
+    "django_browser_reload",
 ]
 
 MIDDLEWARE = [
@@ -54,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'digitalgrievance.urls'
