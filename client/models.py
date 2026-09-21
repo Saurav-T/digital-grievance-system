2570f65ebb3d9280a4103c8e601ce0fe3ad58bd0
Nabin adhikary <nabbuadhikari97@gmail.com>
2026-09-22 00:21:31 +0545

    update: Models

--- client/models.py
+++ client/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.utils import timezone
-from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 
 # ---------------------------------------------------------------------------
 # Custom User
